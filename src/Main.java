a418d8ec4ae28123f4c3c4fd1d15dfa4a0c4cc9e
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-05 00:36:47 +0300

    Направляю ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+
+        int age = 15;
+        if (age >= 18) {
+        System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
+
 }
