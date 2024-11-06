82f88e4d5886dae0fbd8584021441b1fb1c5f0e6
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-06 23:12:27 +0300

    Направляю ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         //task 1
-
+        System.out.println("Task 1");
         int age = 15;
         if (age >= 18) {
-        System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
         }
-    }
 
+
+        //task 2
+        System.out.println("Task 2");
+        int temperature = 3;
+        if (temperature < 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }
+        if (temperature > 5) {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+        
+    }
 }
+
+
